7a1ebd88f2c7e0e195d952663fc18798884254c9
wissalcodes <lw_messikh@esi.dz>
2025-05-14 22:39:40 +0100

    testing app shutdown

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,30 @@ import { FluentLogger } from './logging/fluent-logger.service';
 import { DbLoggingInterceptor } from './logging/db-logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: new FluentLogger(),
-  });
+  try {
+    const app = await NestFactory.create(AppModule, {
+      logger: new FluentLogger(),
+    });
+    app.use(helmet());
+    app.useGlobalPipes(new ValidationPipe());
 
-  app.useGlobalInterceptors(new DbLoggingInterceptor(new FluentLogger()));
-  app.use(helmet());
-  app.useGlobalPipes(new ValidationPipe());
+    // Configure CORS to allow credentials
+    // app.enableCors({
+    //   origin: true, // or specify your frontend URL
+    //   credentials: true,
+    //   allowedHeaders: ['Content-Type', 'Authorization'],
+    //   exposedHeaders: ['set-cookie'],
+    // });
 
-  // Configure CORS to allow credentials
-  app.enableCors({
-    origin: true, // or specify your frontend URL
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['set-cookie'],
-  });
+    const PORT = process.env.PORT || 3000;
+    await app.listen(PORT);
 
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
+    console.log(`Gateway running on ${await app.getUrl()}`);
+  } catch (error) {
+    console.error('Error initializing FluentLogger:', error);
+    process.exit(1); // Gracefully shut down with an error code
+  }
 
-  console.log(`Gateway running on ${await app.getUrl()}`);
+  // app.useGlobalInterceptors(new DbLoggingInterceptor(new FluentLogger()));
 }
 bootstrap();
